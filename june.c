082328a0a3f43b2be278229a0a573abaeb67dc61
pf4 <74263128+elydre@users.noreply.github.com>
2024-12-06 12:25:49 +0100

    update

--- june.c
+++ june.c
@@ -69,10 +69,26 @@ int set_var(char *name, char *value) {
     return 1;
 }
 
-void free_gvar() {
+void free_globals() {
     for (int i = 0; g_vars[i].name; i++) {
         free(g_vars[i].name);
         free(g_vars[i].value);
+    }
+    for (int i = 0; g_rules[i].name; i++) {
+        if (g_rules[i].is_patern) {
+            free(g_rules[i].patern.src_ext);
+            free(g_rules[i].patern.dst_ext);
+        } else {
+            free(g_rules[i].name);
+        }
+        for (int j = 0; g_rules[i].deps[j]; j++)
+            free(g_rules[i].deps[j]);
+        free(g_rules[i].deps);
+        if (!g_rules[i].cmds)
+            continue;
+        for (int j = 0; g_rules[i].cmds[j]; j++)
+            free(g_rules[i].cmds[j]);
+        free(g_rules[i].cmds);
     }
 }
 
@@ -81,6 +97,31 @@ void free_gvar() {
  *      Utility Functions       *
  *                              *
 *********************************/
+
+int is_valid_varname(char *name) {
+    if (!isalpha(*name) && *name != '_')
+        return 0;
+
+    for (int i = 1; name[i]; i++) {
+        if (!isalnum(name[i]) && name[i] != '_')
+            return 0;
+    }
+
+    return 1;
+}
+
+int is_valid_filename(char *name) {
+    for (int i = 0; name[i]; i++) {
+        if (!isalnum(name[i]) &&
+            name[i] != '.' &&
+            name[i] != '_' &&
+            name[i] != '-' &&
+            name[i] != '/'
+        ) return 0;
+    }
+
+    return 1;
+}
 
 char *read_line(FILE *f) {
     char *line = NULL;
@@ -105,10 +146,50 @@ void print_rule(rule_t *rule) {
     for (int i = 0; rule->deps[i]; i++)
         printf("  dep %d: %s\n", i, rule->deps[i]);
 
-    for (int i = 0; rule->cmds[i]; i++)
+    for (int i = 0; rule->cmds && rule->cmds[i]; i++)
         printf("  cmd %d: %s\n", i, rule->cmds[i]);
 
-    printf("\n\n");
+    printf("\n");
+}
+
+char *str_trim(char *str) {
+    int len = strlen(str);
+    while (len > 0 && isspace(str[len - 1]))
+        str[--len] = '\0';
+    return str;
+}
+
+char *str_triml(char *str) {
+    while (isspace(*str))
+        str++;
+    return str;
+}
+
+char **str_split(char *s, char c) {
+    // if consecutive c, only one split
+    // allocate each string
+
+	char	**res;
+	int		start;
+	int		i_tab;
+	int		i;
+
+	i = 0;
+	start = 0;
+	i_tab = 0;
+	res = malloc(sizeof(char *) * (strlen(s) + 1));
+	while (s[i])
+	{
+		while (s[i] && c == s[i])
+			i++;
+		start = i;
+		while (s[i] && c != s[i])
+			i++;
+		if (start < i)
+			res[i_tab++] = strndup(s + start, i - start);
+	}
+	res[i_tab] = NULL;
+	return (res);
 }
 
 /*********************************
@@ -144,17 +225,15 @@ jsf_t g_jsf[] = {
  *     File Interpretation      *
  *                              *
 *********************************/
+
 bool tream_line(char *sline, char **line) {
     bool indent;
     char *tmp;
 
     *line = sline;
 
-    indent = false;
-    while (isspace(**line)) {
-        indent = true;
-        (*line)++;
-    }
+    *line = str_triml(*line);
+    indent = *line != sline;
 
     if ((tmp = strchr(*line, '/')) && tmp[1] == '/')
         *tmp = '\0';
@@ -162,9 +241,13 @@ bool tream_line(char *sline, char **line) {
     if ((tmp = strchr(*line, '#')))
         *tmp = '\0';
 
-    int len = strlen(*line);
-    while (len > 0 && isspace((*line)[len - 1]))
-        (*line)[--len] = '\0';
+    str_trim(*line);
+
+    // replace white spaces with ' '
+    for (int i = 0; (*line)[i]; i++) {
+        if (isspace((*line)[i]))
+            (*line)[i] = ' ';
+    }
 
     return indent;
 }
@@ -225,7 +308,7 @@ char *expand_vars(char *src, int lnb) {
             free(name);
             free(line);
             return NULL;
-        }        
+        }
     }
 
     return line;
@@ -251,35 +334,53 @@ int interp_file(FILE *f) {
             }
             printf("%s\n", line);
 
-            for (int i = 0; line[i]; i++) {
-                if (isspace(line[i]) || line[i] == '=') {
-                    int j = i;
-                    while (isspace(line[j]))
-                        j++;
-                    if (line[j++] != '=')
-                        break;
-                    while (isspace(line[j]))
-                        j++;
-                    if (line[j] == '\0') {
-                        printf("June: line %d: Invalid variable assignment\n", lnb);
-                        free(line);
-                        free(sline);
-                        return 1;
-                    }
-                    char *name = strndup(line, i);
-                    char *value = strdup(line + j);
-                    if (set_var(name, value)) {
-                        printf("June: line %d: Too many variables\n", lnb);
-                        free(name);
-                        free(value);
-                        free(line);
-                        free(sline);
-                        return 1;
-                    }
-                    break;                 
+            char *tmp;
+            if ((tmp = strchr(line, '='))) {
+                *tmp = '\0';
+                char *name = strdup(str_trim(line));
+                if (!is_valid_varname(name)) {
+                    printf("June: line %d: %s: Invalid variable name\n", lnb, name);
+                    free(sline);
+                    free(line);
+                    free(name);
+                    return 1;
                 }
-            }       
+                set_var(name, strdup(str_triml(tmp + 1)));
+            } else if ((tmp = strchr(line, ':'))) {
+                *tmp = '\0';
+                char *name = strdup(str_trim(line));
+                if (!is_valid_filename(name)) {
+                    printf("June: line %d: %s: Invalid rule name\n", lnb, name);
+                    free(sline);
+                    free(line);
+                    free(name);
+                    return 1;
+                }
 
+                char **deps = str_split(tmp + 1, ' ');
+                for (int i = 0; deps[i]; i++) {
+                    if (!is_valid_filename(deps[i])) {
+                        printf("June: line %d: %s: Invalid dependency name\n", lnb, deps[i]);
+                        free(sline);
+                        free(line);
+                        for (int j = 0; deps[j]; j++)
+                            free(deps[j]);
+                        free(deps);
+                        free(name);
+                        return 1;
+                    }
+                }
+
+                rule_t *rule = g_rules;
+                for (; rule->name; rule++);
+                rule->name = name;
+                rule->deps = deps;
+            } else {
+                printf("June: line %d: Invalid statement\n", lnb);
+                free(sline);
+                free(line);
+                return 1;
+            }
             free(line);
         } else {
             printf("    %s\n", line);
@@ -315,6 +416,7 @@ int main(int argc, char **argv) {
 
     if (interp_file(f)) {
         fclose(f);
+        free_globals();
         free(g_rules);
         free(g_vars);
         return 1;
@@ -322,10 +424,11 @@ int main(int argc, char **argv) {
 
     fclose(f);
 
+    printf("\n============ Rules ============\n");
     for (int i = 0; g_rules && g_rules[i].name; i++)
         print_rule(g_rules + i);
 
-    free_gvar();
+    free_globals();
     free(g_rules);
     free(g_vars);
 
